a4c14cbfc2ac3b60b1916681a7cdc1eed0804511
rah-ds <87576921+rah-ds@users.noreply.github.com>
2024-12-02 16:10:56 -0500

    eod

--- src/plots.py
+++ src/plots.py
@@ -1,7 +1,12 @@
+import sys
+
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+
+sys.path.append("..//")
+from src.make_data import build_metrics
 
 colors_zebra_interview = {
     "Vibrant Purple": "#574cfa",
@@ -16,25 +21,27 @@ colors_zebra_interview = {
     "Warm Gray": "#cecece"
 } # taken from attachments given in interview, used chatGPT to get hexidecimal 
 
-def plot_median_movie_score_over_time(df:pd.DataFrame, colors_zebra_interview = colors_zebra_interview) -> matplotlib.figure.Figure:
+def plot_median_movie_score_over_time(df:pd.DataFrame) -> matplotlib.figure.Figure:
     """gives the median movie score over time, returns the figure."""
 
     df_tmp = df.loc[df["movie_score"].notnull(), ].copy(deep=True)
+    df_tmp.loc[df_tmp["decade"] < 1960, "decade"]= 1960
     df_tmp = df_tmp.assign(decade = df_tmp.decade.astype(str).str.replace("9990", "N/As")) # make sure nulls on right side of graph
-
-    df_tmp.loc[df["decade"] < 1960, "decade"]= 1960
 
     tbl = df_tmp.groupby("decade").agg(
         n = ("movie_title", "count"),
         median_score = ("movie_score", "median"),
+        movie_score = ("movie_score", "sum"),
+        gross = ("gross", "sum"),
+        budget = ("budget", "sum"),
         avg_users_voted = ("num_users_voted", "mean"),
-        median_lift = ("lift", "median"),
-        median_gross = ("gross", "median")
-    ).reset_index()
+        num_users_voted = ("num_users_voted", "sum")
+    ).round(2)
 
+    tbl = tbl.reset_index()
+    tbl = build_metrics(tbl)
 
     fig, ax = plt.subplots()
-    tbl = tbl.assign(decade = tbl.decade.astype(str).str.replace("9990", "N/As")) # make sure nulls on right side of graph
 
     sns.barplot(data = tbl, x = "decade", y = "n", ax = ax,
                  color=colors_zebra_interview["Deep Blue"]).set(title = "Median Movie Score by Decade", ylabel ="Count of Movies in Data")
@@ -45,30 +52,35 @@ def plot_median_movie_score_over_time(df:pd.DataFrame, colors_zebra_interview = 
                    color=colors_zebra_interview["Vibrant Purple"],
                    label = "Median Movie Rating (1-10) by decade").set(ylabel = "Median Score (1-10)")
     
-    plt.axhline(y=df["movie_score"].median(), color=colors_zebra_interview["Light Gray"], linestyle='--', linewidth=1.5, label = "Median Overall") # doesn't make sense take from the raw
+    ax2.set_ylim(1,10)
+    plt.axhline(y=tbl["median_score"].median(), color=colors_zebra_interview["Light Gray"], linestyle='--', linewidth=1.5, label = "Median Overall") # doesn't make sense take from the raw
 
     plt.tight_layout()
     plt.savefig("..//plots/median_movie_score.png")
 
     return fig
 
-def plot_median_movie_lift_over_time(df:pd.DataFrame, colors_zebra_interview = colors_zebra_interview) -> matplotlib.figure.Figure:
+def plot_median_movie_lift_over_time(df:pd.DataFrame) -> matplotlib.figure.Figure:
     """
     gives the median movie lift over time, saves out and gives  a figure to be rendered."""
 
-    df_tmp = df.loc[df["lift"].notnull(), ].copy(deep=True)
+    df_tmp = df.loc[df["movie_score"].notnull(), ].copy(deep=True)
+    df_tmp.loc[df_tmp["decade"] < 1960, "decade"]= 1960
     df_tmp = df_tmp.assign(decade = df_tmp.decade.astype(str).str.replace("9990", "N/As")) # make sure nulls on right side of graph
-
-    df_tmp.loc[df["decade"] < 1960, "decade"]= 1960
 
     tbl = df_tmp.groupby("decade").agg(
         n = ("movie_title", "count"),
         median_score = ("movie_score", "median"),
+        movie_score = ("movie_score", "sum"),
+        gross = ("gross", "sum"),
+        budget = ("budget", "sum"),
         avg_users_voted = ("num_users_voted", "mean"),
-        median_lift = ("lift", "median"),
-        median_gross = ("gross", "median")
-    ).reset_index()
+        num_users_voted = ("num_users_voted", "sum")
+    ).round(2)
 
+    tbl = tbl.reset_index()
+    tbl = build_metrics(tbl)
+    
 
     fig, ax = plt.subplots()
     tbl = tbl.assign(decade = tbl.decade.astype(str).str.replace("9990", "N/As")) # make sure nulls on right side of graph
@@ -78,11 +90,11 @@ def plot_median_movie_lift_over_time(df:pd.DataFrame, colors_zebra_interview = c
 
     ax2 = ax.twinx()
 
-    sns.pointplot(data = tbl, x="decade", y = "median_lift", ax = ax2,
+    sns.pointplot(data = tbl, x="decade", y = "lift", ax = ax2,
                    color=colors_zebra_interview["Light Silver"],
                    label = "lift").set(ylabel = "lift = gross / budget")
     
-    plt.axhline(y=df["lift"].median(), color=colors_zebra_interview["Warm Gray"], linestyle='--', linewidth=1.5, label = "Median Overall") # doesn't make sense take from the raw
+    plt.axhline(y=tbl["lift"].median(), color=colors_zebra_interview["Warm Gray"], linestyle='--', linewidth=1.5, label = "Median Overall") # doesn't make sense take from the raw
 
     plt.tight_layout()
     plt.savefig("..//plots/median_lift_decade.png")
--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,10 +1,44 @@
-import numpy as np
+import sys
 
-## include some edge cases and how it should react to certain assumptions about the data
+from dotenv import find_dotenv, dotenv_values
+import pandas as pd
+import pytest
+
+sys.path.append("./")
+from src.make_data import clean_and_normalize_movie_title
 
 
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    """Load the .env file before running tests."""
+    config = dotenv_values(find_dotenv(".env"))
+    return config
 
-def test_correct_file():
-    assert True
 
-    
+def load_test_movie_data():
+    return pd.read_csv(config["data_path_cleaned"]) 
+
+
+# test titles individually
+@pytest.mark.parametrize("input_title, expected_output", [
+    (" Inception", "inception"),  # Leading spaces
+    ("Inception ", "inception"),  # Trailing spaces
+    (" Inception ", "inception"),  # Leading/Trailing spaces
+    ("The   Dark   Knight", "the dark knight"),  # Multiple spaces
+    ("Pulp Fiction!", "pulp fiction!"),  # Valid punctuation
+    ("Shrek 2", "shrek 2"),  # Numbers preserved
+    ("Lord of the Rings: The Two Towers", "lord of the rings the two towers"),  # Colon removed (franchise should be captured later)
+    ("  A Beautiful Mind ", "a beautiful mind"),  # Combination of trim and normalization
+    ("Spider-Man", "spider-man")  # Dash preserved
+])
+def test_clean_and_normalize_movie_title(input_title, expected_output):
+    """comments about what to expect inline"""
+    assert clean_and_normalize_movie_title(input_title) == expected_output
+
+def test_empty_string():
+    """Test handling of an empty string"""
+    assert clean_and_normalize_movie_title("") == ""
+
+def test_only_special_characters():
+    """Test handling of a string with only invalid characters"""
+    assert clean_and_normalize_movie_title("@#$") == ""
